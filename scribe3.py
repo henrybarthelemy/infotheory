a5703d59a26e29b5acdb3fc45aa86bb1e598f0ec
henry <barthelemy.h@northeastern.edu>
2024-11-21 00:36:39 +0000

    Centroids going as a movie, might redo to also save images for iterations

--- scribe3.py
+++ scribe3.py
@@ -9,17 +9,33 @@ class LloydsAlgorithm(Scene):
         num_iterations = 4
 
         # Generate random points in 2D space
-        points_x = np.random.rand(num_points) * 6 - 3  
-        points_y = np.random.rand(num_points) * 6 - 3  
-        
+        points_x = np.random.rand(num_points) * 6 - 3
+        points_y = np.random.rand(num_points) * 6 - 3
+        points = np.column_stack((points_x, points_y))
 
         # Create a group of dots at the random coordinates
-        dots = VGroup(*[Dot(point=(coord[0], coord[1], 0)) for coord in np.column_stack((points_x, points_y))])
-
-        # Create point dots in Manim
-        # point_dots = VGroup(*[Dot(point=coords) for coords in points])
+        dots = VGroup(*[Dot(point=(coord[0], coord[1], 0), color=WHITE) for coord in points])
         self.add(dots)
 
+        # Initialize centroids and create centroid dots
+        centroids = initialize_centroids(points, num_clusters)
+        centroid_dots = VGroup(*[Dot(point=(coord[0], coord[1], 0), color=YELLOW).scale(1.2) for coord in centroids])
+        self.add(centroid_dots)
+
+        for _ in range(num_iterations):
+            # Assign points to nearest centroids and color them
+            labels = assign_clusters(points, centroids)
+            for i, dot in enumerate(dots):
+                dot.set_color([RED, BLUE, GREEN][labels[i]])  # Colors for each cluster
+
+            # Update centroid positions and animate the movement of centroids
+            new_centroids = update_centroids(points, labels, num_clusters)
+            for j, (centroid_dot, new_position) in enumerate(zip(centroid_dots, new_centroids)):
+                self.play(centroid_dot.animate.move_to((new_position[0], new_position[1], 0)), run_time=1)
+
+            # Update centroids for the next iteration
+            centroids = new_centroids
+            self.wait(0.5)  # Pause to observe each iteration
 
 def initialize_centroids(X, k):
     """Randomly initialize k centroids from the dataset."""
@@ -33,21 +49,4 @@ def assign_clusters(X, centroids):
 
 def update_centroids(X, labels, k):
     """Update centroid positions as the mean of assigned points."""
-    new_centroids = np.array([X[labels == i].mean(axis=0) for i in range(k)])
-    return new_centroids
-
-def lloyds_algorithm(X, k, max_iters=100, tol=1e-4):
-    """Performs K-means clustering using Lloyd's algorithm."""
-    centroids = initialize_centroids(X, k)
-    
-    for i in range(max_iters):
-        old_centroids = centroids
-        labels = assign_clusters(X, centroids)
-        centroids = update_centroids(X, labels, k)
-
-        # Check for convergence
-        if np.all(np.linalg.norm(centroids - old_centroids, axis=1) < tol):
-            break
-            
-    return labels, centroids
-
+    return np.array([X[labels == i].mean(axis=0) for i in range(k)])
